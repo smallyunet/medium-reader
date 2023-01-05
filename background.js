a57e5ef56e557d69f100be82b916b16c869cad6d
smallyunet <dev.wangyu@gmail.com>
2023-01-05 14:35:02 +0800

    fix search page

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.url && changeInfo.url.includes("medium.com")) {
+  let r1 = changeInfo.url
+  let r2 = changeInfo.url.includes("medium.com")
+  let r3 = !changeInfo.url.includes("search?")
+  if (r1 && r2 && r3) {
     chrome.tabs.reload(tabId);
   }
 });
