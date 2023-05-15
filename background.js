5859227ccd30b69412d66d3ad63c490429cf7715
smallyunet <dev.wangyu@gmail.com>
2023-05-15 14:16:21 +0800

    fix

--- background.js
+++ background.js
@@ -1,23 +1,23 @@
-const rs = [
-  "medium.com",
-  "javascript.plainenglish.io",
-  "medium.datadriveninvestor.com",
-];
+let logEnabled = true; // 日志开关
+
+function log(...args) {
+  if (logEnabled) {
+    console.log(...args);
+  }
+}
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  console.log('onUpdated event triggered');
-  console.log('URL:', changeInfo.url);
+  log('onUpdated event triggered');
+  log('URL:', changeInfo.url);
   if (changeInfo.url && !changeInfo.url.includes("search?")) {
     const url = new URL(changeInfo.url);
-    console.log('Hostname:', url.hostname);
-    console.log('Pathname:', url.pathname);
-    const hostnameMatch = rs.some((r) => url.hostname.includes(r));
-    console.log('Hostname match:', hostnameMatch);
+    log('Hostname:', url.hostname);
+    log('Pathname:', url.pathname);
     let pathnameMatch = url.pathname.split('/').length > 2;
     pathnameMatch = pathnameMatch || url.pathname.includes('/@');
-    console.log('Pathname match:', pathnameMatch);
-    if (hostnameMatch && pathnameMatch) {
-      console.log('Condition met');
+    log('Pathname match:', pathnameMatch);
+    if (pathnameMatch) {
+      log('Condition met');
       chrome.tabs.reload(tabId);
     }
   }
